3d33f04347afc45ca83ba5a09e75fb068e134077
Pavel Barysevich <pavelbarysevich@gmail.com>
2026-01-14 15:09:46 +0300

    КОММИТИРОВАТЬ СООБЩЕНИЯ

--- TestGit/ContentView.swift
+++ TestGit/ContentView.swift
@@ -5,6 +5,32 @@
 //  Created by Павел Борисевич on 13.01.26.
 //
 
+/*
+ COMMIT MESSAGES
+ NEW FEATURE:
+ [Featurel Description of the feature
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ BUG NOT IN PRODUCTLON:
+ [Bug] Description of the bug
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ RELEASE:
+ [Release] Description of release
+ 
+ КОММИТИРОВАТЬ СООБЩЕНИЯ
+  НОВАЯ ФУНКЦИЯ:
+  [Функция Описание функции
+  ОШИБКА В ПРОИЗВОДСТВЕ:
+  [Патч] Описание патча
+  ОШИБКА НЕ В ПРОДУКТЕ:
+  [Ошибка] Описание ошибки
+  ОБЫЧНЫЕ ЗАДАЧИ:
+  [Очистить] Описание изменений
+  РЕЛИЗ:
+  [Релиз] Описание релиза
+ */
+
 import SwiftUI
 
 struct ContentView: View {
