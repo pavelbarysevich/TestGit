da6a78846c59f3868f809400d570a1d2c74c5cab
Pavel Barysevich <pavelbarysevich@gmail.com>
2026-01-23 11:21:41 +0300

    [Feature] Add doSomething state

--- TestGit/SettingSView.swift
+++ TestGit/SettingSView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 
 struct SettingSView: View {
+    
+    @State private var doSomething: Bool = false
+    
     var body: some View {
-        Text("Настройки")
+        Text("Настройки!")
     }
 }
 
